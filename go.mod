fee0ecaea0c4c6c750bedff06bf6fc74be8b745c
ViviMemory <WhatOpenSource@yandex.ru>
2022-03-06 04:41:00 +0300

    add send sms phone

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/heroku/x v0.0.33
 	github.com/lib/pq v1.10.4
+	github.com/twilio/twilio-go v0.22.1
 )
 
 require (
@@ -14,15 +15,17 @@ require (
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -27,6 +31,56 @@ func repeatHandler(r int) gin.HandlerFunc {
 
 func dbFunc(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		// Set account keys & information
+		accountSid := "ACaad6ab76876e7822323bbe3f91106810"
+		authToken := "97214b0dd56c113e81d4524cad8545a8"
+		urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+
+		// Create possible message bodies
+		quotes := "Йоу, вот твой код смс для входа в приложение\n 342121"
+
+		// Set up rand
+		rand.Seed(time.Now().Unix())
+
+		msgData := url.Values{}
+		msgData.Set("To", "+79674781443")
+		msgData.Set("From", "+13607039136")
+		msgData.Set("Body", quotes)
+		msgDataReader := *strings.NewReader(msgData.Encode())
+
+		client := &http.Client{}
+		req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+		req.SetBasicAuth(accountSid, authToken)
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		resp, _ := client.Do(req)
+		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+			var data map[string]interface{}
+			decoder := json.NewDecoder(resp.Body)
+			err := decoder.Decode(&data)
+			if err == nil {
+				fmt.Println(data["sid"])
+			}
+		} else {
+			fmt.Println(resp.Status)
+		}
+
+		//client := twilio.NewRestClient()
+
+		//params := &openapi.CreateMessageParams{}
+		//params.SetTo("+79674781443")
+		//params.SetFrom("+13607039136")
+		//params.SetBody("Hello from Golang!")
+		//
+		//_, err := client.ApiV2010.CreateMessage(params)
+		//if err != nil {
+		//	fmt.Println(err.Error())
+		//} else {
+		//	fmt.Println("SMS sent successfully!")
+		//}
+
 		if _, err := db.Exec("drop table users"); err != nil {
 			c.String(http.StatusInternalServerError,
 				fmt.Sprintf("Error creating database table: %q", err))
@@ -69,7 +123,8 @@ func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = "8080"
+		//log.Fatal("$PORT must be set")
 	}
 
 	tStr := os.Getenv("REPEAT")
