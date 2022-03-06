45ec7e2c74b9fc9b3b141ae3dfe5904eaff80453
ViviMemory <WhatOpenSource@yandex.ru>
2022-03-06 05:50:49 +0300

    fix

--- main.go
+++ main.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"bytes"
-	"database/sql"
-	"encoding/json"
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/heroku/go-getting-started/pkg/repository"
 	service2 "github.com/heroku/go-getting-started/pkg/service"
+	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"log"
-	"math/rand"
 	"net/http"
-	"net/url"
 	"os"
 	"strconv"
-	"strings"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
-	_ "github.com/lib/pq"
 )
 
 func repeatHandler(r int) gin.HandlerFunc {
@@ -32,95 +24,95 @@ func repeatHandler(r int) gin.HandlerFunc {
 	}
 }
 
-func dbFunc(db *sqlx.DB) gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		// Set account keys & information
-		accountSid := "ACaad6ab76876e7822323bbe3f91106810"
-		authToken := "97214b0dd56c113e81d4524cad8545a8"
-		urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
-
-		// Create possible message bodies
-		quotes := "Йоу, вот твой код смс для входа в приложение\n 342121"
-
-		// Set up rand
-		rand.Seed(time.Now().Unix())
-
-		msgData := url.Values{}
-		msgData.Set("To", "+79674781443")
-		msgData.Set("From", "+13607039136")
-		msgData.Set("Body", quotes)
-		msgDataReader := *strings.NewReader(msgData.Encode())
-
-		client := &http.Client{}
-		req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
-		req.SetBasicAuth(accountSid, authToken)
-		req.Header.Add("Accept", "application/json")
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-		resp, _ := client.Do(req)
-		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-			var data map[string]interface{}
-			decoder := json.NewDecoder(resp.Body)
-			err := decoder.Decode(&data)
-			if err == nil {
-				fmt.Println(data["sid"])
-			}
-		} else {
-			fmt.Println(resp.Status)
-		}
-
-		//client := twilio.NewRestClient()
-
-		//params := &openapi.CreateMessageParams{}
-		//params.SetTo("+79674781443")
-		//params.SetFrom("+13607039136")
-		//params.SetBody("Hello from Golang!")
-		//
-		//_, err := client.ApiV2010.CreateMessage(params)
-		//if err != nil {
-		//	fmt.Println(err.Error())
-		//} else {
-		//	fmt.Println("SMS sent successfully!")
-		//}
-
-		if _, err := db.Exec("drop table users"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
-			return
-		}
-
-		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error creating database table: %q", err))
-			return
-		}
-
-		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error incrementing tick: %q", err))
-			return
-		}
-
-		rows, err := db.Query("SELECT tick FROM ticks")
-		if err != nil {
-			c.String(http.StatusInternalServerError,
-				fmt.Sprintf("Error reading ticks: %q", err))
-			return
-		}
-
-		defer rows.Close()
-		for rows.Next() {
-			var tick time.Time
-			if err := rows.Scan(&tick); err != nil {
-				c.String(http.StatusInternalServerError,
-					fmt.Sprintf("Error scanning ticks: %q", err))
-				return
-			}
-			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
-		}
-	}
-}
+//func dbFunc(db *sqlx.DB) gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//
+//		// Set account keys & information
+//		accountSid := "ACaad6ab76876e7822323bbe3f91106810"
+//		authToken := "97214b0dd56c113e81d4524cad8545a8"
+//		urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+//
+//		// Create possible message bodies
+//		quotes := "Йоу, вот твой код смс для входа в приложение\n 342121"
+//
+//		// Set up rand
+//		rand.Seed(time.Now().Unix())
+//
+//		msgData := url.Values{}
+//		msgData.Set("To", "+79674781443")
+//		msgData.Set("From", "+13607039136")
+//		msgData.Set("Body", quotes)
+//		msgDataReader := *strings.NewReader(msgData.Encode())
+//
+//		client := &http.Client{}
+//		req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+//		req.SetBasicAuth(accountSid, authToken)
+//		req.Header.Add("Accept", "application/json")
+//		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+//
+//		resp, _ := client.Do(req)
+//		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+//			var data map[string]interface{}
+//			decoder := json.NewDecoder(resp.Body)
+//			err := decoder.Decode(&data)
+//			if err == nil {
+//				fmt.Println(data["sid"])
+//			}
+//		} else {
+//			fmt.Println(resp.Status)
+//		}
+//
+//		//client := twilio.NewRestClient()
+//
+//		//params := &openapi.CreateMessageParams{}
+//		//params.SetTo("+79674781443")
+//		//params.SetFrom("+13607039136")
+//		//params.SetBody("Hello from Golang!")
+//		//
+//		//_, err := client.ApiV2010.CreateMessage(params)
+//		//if err != nil {
+//		//	fmt.Println(err.Error())
+//		//} else {
+//		//	fmt.Println("SMS sent successfully!")
+//		//}
+//
+//		if _, err := db.Exec("drop table users"); err != nil {
+//			c.String(http.StatusInternalServerError,
+//				fmt.Sprintf("Error creating database table: %q", err))
+//			return
+//		}
+//
+//		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+//			c.String(http.StatusInternalServerError,
+//				fmt.Sprintf("Error creating database table: %q", err))
+//			return
+//		}
+//
+//		if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+//			c.String(http.StatusInternalServerError,
+//				fmt.Sprintf("Error incrementing tick: %q", err))
+//			return
+//		}
+//
+//		rows, err := db.Query("SELECT tick FROM ticks")
+//		if err != nil {
+//			c.String(http.StatusInternalServerError,
+//				fmt.Sprintf("Error reading ticks: %q", err))
+//			return
+//		}
+//
+//		defer rows.Close()
+//		for rows.Next() {
+//			var tick time.Time
+//			if err := rows.Scan(&tick); err != nil {
+//				c.String(http.StatusInternalServerError,
+//					fmt.Sprintf("Error scanning ticks: %q", err))
+//				return
+//			}
+//			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+//		}
+//	}
+//}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -137,7 +129,7 @@ func main() {
 		repeat = 5
 	}
 
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	db, err := sqlx.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("Error opening database: %q", err)
 	}
@@ -158,7 +150,7 @@ func main() {
 
 	router.GET("/repeat", repeatHandler(repeat))
 
-	router.GET("/db", dbFunc(db))
+	//router.GET("/db", dbFunc(db))
 
 	router.Run(":" + port)
 }
